1072535581f673b4bcc271e7298602fbf5785c46
Julia <julia@kptncook.com>
2020-06-21 12:13:56 +0200

    Adjust DBSCAN parameters to 2d feature space - Outlier Calculation

--- algorithms/dbscan-outlier-detection.py
+++ algorithms/dbscan-outlier-detection.py
@@ -5,10 +5,10 @@ from collections import Counter
 
 # Reading in 2D Feature Space
 feature_space = pd.read_csv("../datasets/wine-data.csv", header=None, sep=",")
-data = feature_space.iloc[:, 0:1].values
+data = feature_space.iloc[:, 0:2].values
 
 # DBSCAN model with parameters
-model = DBSCAN(eps=0.2, min_samples=19).fit(data)
+model = DBSCAN(eps=0.8, min_samples=10).fit(data)
 
 # Creating Panda DataFrame with Labels for Outlier Detection
 outlier_df = pd.DataFrame(data)
